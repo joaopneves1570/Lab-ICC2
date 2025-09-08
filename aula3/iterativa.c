d180c228432e716c5e2b212d4291f2311d0f4dab
Juan <juanptomba@gmail.com>
2025-09-08 13:16:24 -0300

    Adição de comentários

--- aula3/iterativa.c
+++ aula3/iterativa.c
@@ -7,15 +7,16 @@ int main(){
         printf("0");
         return 0;
     }
+    // Caso a potência seja nula (N⁰ = 1)
     if (k == 0){
         printf("%d", rfinal);
         return 0;
     }
-
+    // Só utiliza os 3 último números
     for (int i = 0; i < k; i++){
         rfinal = (rfinal * n)%1000;
     }
-    rfinal = (rfinal * rfinal) %1000;
+    
     
     printf("%d", rfinal);
 
